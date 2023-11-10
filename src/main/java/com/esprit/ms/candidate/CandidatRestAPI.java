560503982f632c7e26426274711e42587a69cff0
InesMjd <47245534+InesMjd@users.noreply.github.com>
2023-11-10 13:27:17 +0100

    Update CandidatRestAPI.java

--- src/main/java/com/esprit/ms/candidate/CandidatRestAPI.java
+++ src/main/java/com/esprit/ms/candidate/CandidatRestAPI.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/candidats")
 public class CandidatRestAPI {
     private String title="Hello, i'm the candidate Micro Service";
 
@@ -18,6 +19,11 @@ public class CandidatRestAPI {
 
     @Autowired
     private CandidatService candidatService;
+
+    @RequestMapping
+    public List<Candidat> getListCandid() {
+        return candidatService.getAll();
+    }
 
     @PostMapping(consumes = MediaType.APPLICATION_XML_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
